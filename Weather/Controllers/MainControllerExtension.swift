dd333bb3a7b450e3ec1cdfbb52a7e30e6983591c
Subheksha Gautam <subhekshagautam@gmail.com>
2022-07-27 22:43:07 +1000

    *Mainview controller extension separate file is created *hide keyboard and on tap gesture added * code refactored *UI of dropdown changed

---
+++ Weather/Controllers/MainControllerExtension.swift
@@ -0,0 +1,151 @@
+//
+//  MainControllerExtension.swift
+//  Weather
+//
+//  Created by Subheksha Gautam on 27/7/2022.
+//
+
+import CoreLocation
+import UIKit
+
+
+//MARK: - UItextfield padding and image adjustment
+extension  UITextField{
+    func setLeftImage(imageName: String) {
+        self.leftView = UIView(frame: CGRect(x: 10, y: 0, width: 30, height: 40))
+        self.leftViewMode = .always
+        let leftViewItSelf = UIImageView(frame: CGRect(x: 10, y: 10, width: 20, height: 20))
+        leftViewItSelf.image = UIImage(systemName: imageName)
+        self.tintColor = .white
+        
+        leftView?.addSubview(leftViewItSelf)
+    }
+}
+
+//MARK: - cllocationmanager delegate
+
+extension MainViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            print("lat: \(lat), long: \(lon)")
+            
+            // show location icon here
+            self.locationImage.isHidden = false
+            
+            // API call using user current location ie. lat/long
+            self.showSpinner(onView: self.view)
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
+
+//MARK: - weather manager delegeate, Get and show todays weather
+
+extension MainViewController: WeatherManagerDelegate {
+    
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        
+        //Here you get API response from first API call ie from WeatherManager
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImage.image = UIImage(systemName: weather.conditionName)
+            self.cityLabel.text = weather.cityName
+            
+            // Make main backgroud dynamic
+            self.mainBackground.image = UIImage(named: weather.backgroundName)
+            
+            // We make second API call to get 7 days of weatehr report
+            // using DailyWeatherManager to fetch 7 days of weather.
+            self.dailyWeatherManager.performRequestFor7DaysWeather(latitude: weather.latitude,longitude: weather.longitude)
+        }
+        //Saving only from sucess cities response on user defaults
+        // if dropDownArray count >=10 remove 0 position city and append new city
+        if !dropDownArray.contains(weather.cityName){
+            if dropDownArray.count >= 10{
+                dropDownArray.remove(at: 0)
+            }
+            dropDownArray.append (weather.cityName.capitalized)
+        }
+        
+        defaults.set(self.dropDownArray,forKey: userDefaultKey)
+        for items in dropDownArray{
+            print("default ======== \(items)")
+        }
+    }
+    func didFailWithError(error: Error) {
+        print("Got error response")
+        self.removeSpinner()
+    }
+}
+
+//MARK: - TableView Datasource
+extension MainViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allDaysData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MainViewTableCell", for: indexPath)
+        as! MainViewTableCell
+        
+        let data = allDaysData[indexPath.row]
+        let weatherModel = WeatherModel(
+            conditionId: data.weather[0].id,
+            cityName: "",
+            temperature : 0,
+            latitude : 0,
+            longitude: 0)
+        cell.conditionImage.image = UIImage(systemName: weatherModel.conditionName)
+        
+        // convert miliseconds into date fromat
+        let date = Date(timeIntervalSince1970: (Double(data.dt)))
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "E, d MMM"
+        cell.dateLabel.text = String((dateFormatter.string(from: date)))
+        
+        cell.minTempLabel.text = String (format: "%.0f", data.temp.min)
+        cell.maxTempLabel.text = String (format: "%.0f", data.temp.max)
+        
+        cell.isUserInteractionEnabled = false
+        tableView.alwaysBounceVertical = false
+        return cell
+    }
+}
+
+//MARK: - Get daily response 8days
+extension MainViewController: DailyWeatherManagerDelegate{
+    func didFailWithError(error: Error?, message: String?) {
+        print("Something is wrong: Error:\(error.debugDescription) ||| Message:\(message ?? "")")
+        self.removeSpinner()
+        
+        //TODO: show error dialog box with generic message
+    }
+    
+    func didGetAllResponse(dailyData: DailyWeatherData) {
+        allDaysData = dailyData.daily
+        allDaysData.removeFirst()
+        
+        DispatchQueue.main.async {
+            self.predictionTable.reloadData()
+            self.removeSpinner()
+        }
+    }
+}
+
+//MARK: - To hide keyword when user tab on the views
+extension UIViewController {
+    func hideKeyboardWhenTappedAround(){
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyword))
+        view.addGestureRecognizer(tapGesture)
+    }
+    @objc func hideKeyword(){
+        view.endEditing(true)
+    }
+}
--- Weather/Controllers/MainViewController.swift
+++ Weather/Controllers/MainViewController.swift
@@ -21,7 +21,7 @@ class MainViewController: UIViewController {
     @IBOutlet weak var predictionTable: UITableView!
     
     
-    private var allData = [Daily]()
+    var allDaysData = [Daily]()
     var dailyWeatherManager = DailyWeatherManager()
     var weatherManager = WeatherManager()
     let locationManager = CLLocationManager()
@@ -42,8 +42,6 @@ class MainViewController: UIViewController {
         
         super.viewDidLoad()
         
-        dropDownArray = defaults.array(forKey: userDefaultKey) as? [String] ?? dropDownArray
-        
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.requestLocation()
@@ -59,19 +57,27 @@ class MainViewController: UIViewController {
         searchField.delegate = self
         dailyWeatherManager.delegate = self
         
+        dropDownArray = defaults.array(forKey: userDefaultKey) as? [String] ?? dropDownArray
+        
         // dropdown part
         dropDown.anchorView = searchField
         dropDown.dataSource = dropDownArray
         dropDown.bottomOffset = CGPoint(x: 0, y:(dropDown.anchorView?.plainView.bounds.height)!)
         dropDown.topOffset = CGPoint(x: 0, y:-(dropDown.anchorView?.plainView.bounds.height)!)
         dropDown.direction = .bottom
-        DropDown.appearance().backgroundColor = UIColor(red: 0.00, green: 0.42, blue: 0.46, alpha: 0.50)
-        DropDown.appearance().selectionBackgroundColor = UIColor.lightGray.withAlphaComponent(0.4)
+        DropDown.appearance().cornerRadius = 16
+        DropDown.appearance().backgroundColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 0.70)
+        DropDown.appearance().selectionBackgroundColor = UIColor.lightGray.withAlphaComponent(0.60)
         dropDown.reloadAllComponents()
         
-        print(dropDownArray.count)
-        self.didSelectInMenuCallBack = { [weak self] selectedCity
-            in
+        // Action triggered on dropdown item selection
+        dropDown.selectionAction = { [unowned self] (index: Int, item: String) in
+            hideKeyword()
+            // API calling using city name
+            apiCallByCityName(city: dropDownArray[index])
+        }
+        
+        self.didSelectInMenuCallBack = { [weak self] selectedCity in
             guard self != nil else { return }
             
             print(selectedCity)
@@ -81,6 +87,9 @@ class MainViewController: UIViewController {
             // Hit api here
             self!.apiCallByCityName(city: selectedCity)
         }
+        
+        //Hiding keyword when user taps on main screen
+        hideKeyboardWhenTappedAround()
     }
     
     @IBAction func sideMenuPressed(_ sender: Any) {
@@ -88,73 +97,31 @@ class MainViewController: UIViewController {
     }
 }
 
-//MARK: - UItextfield padding and image adjustment
-extension  UITextField{
-    func setLeftImage(imageName: String) {
-        self.leftView = UIView(frame: CGRect(x: 10, y: 0, width: 30, height: 40))
-        self.leftViewMode = .always
-        let leftViewItSelf = UIImageView(frame: CGRect(x: 10, y: 10, width: 20, height: 20))
-        leftViewItSelf.image = UIImage(systemName: imageName)
-        self.tintColor = .white
-        
-        leftView?.addSubview(leftViewItSelf)
-    }
-}
 
 //MARK: - UI TextField Delegate
-
 extension MainViewController: UITextFieldDelegate {
     
+    //calling for the first time when user clicks on UITextField
     func textFieldDidBeginEditing(_ textField: UITextField) {
         // if dropdown is showing update else show
         dropDownArray = defaults.array(forKey: userDefaultKey) as? [String] ?? dropDownArray
         dropDown.dataSource = dropDownArray
         dropDown.show()
-        
-        // Action triggered on dropdown item selection
-        dropDown.selectionAction = { [unowned self] (index: Int, item: String) in
-            // API calling using city name
-            apiCallByCityName(city: dropDownArray[index])
-        }
-        
     }
     
     // return when user press return keyword in the keyword
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         searchField.endEditing(true)
         dropDown.hide()
+        hideKeyword()
         return true
     }
-    
-    // useful for validation on what user types
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        if textField.text != "" {
-            return true
-        }
-        else {
-            textField.placeholder = "Type Something Here"
-            return false
-        }
-    }
-    
     //delete text fiels contents after user types search or return keyword
     func textFieldDidEndEditing(_ textField: UITextField) {
         //textfiled.text data passed by user need to be hold
         if let city = searchField.text?.trimmingCharacters(in: .whitespacesAndNewlines){
-            
-            apiCallByCityName(city: city)
-            
-            // if dropDownArray count >=10 remove 0 position city and append new city
-            if !dropDownArray.contains(city){
-                if dropDownArray.count >= 10{
-                    dropDownArray.remove(at: 0)
-                }
-                dropDownArray.append (city.capitalized)
-            }
-            
-            defaults.set(self.dropDownArray,forKey: userDefaultKey)
-            for items in dropDownArray{
-                print("default ======== \(items)")
+            if !city.isEmpty{
+                apiCallByCityName(city: city)
             }
         }
         
@@ -173,114 +140,3 @@ extension MainViewController: UITextFieldDelegate {
     }
     
 }
-
-//MARK: - weather manager delegeate
-
-extension MainViewController: WeatherManagerDelegate {
-    
-    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
-        
-        //Here you get API response from first API call ie from WeatherManager
-        DispatchQueue.main.async {
-            self.temperatureLabel.text = weather.temperatureString
-            self.conditionImage.image = UIImage(systemName: weather.conditionName)
-            self.cityLabel.text = weather.cityName
-            
-            // Make main backgroud dynamic
-            self.mainBackground.image = UIImage(named: weather.backgroundName)
-            
-            // We make second API call to get 7 days of weatehr report
-            // using DailyWeatherManager to fetch 7 days of weather.
-            self.dailyWeatherManager.performRequestFor7DaysWeather(latitude: weather.latitude,longitude: weather.longitude)
-        }
-    }
-    func didFailWithError(error: Error) {
-        print("Got error response")
-        self.removeSpinner()
-    }
-}
-
-//MARK: - cllocationmanager delegate
-
-extension MainViewController: CLLocationManagerDelegate {
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.last {
-            locationManager.stopUpdatingLocation()
-            let lat = location.coordinate.latitude
-            let lon = location.coordinate.longitude
-            print("lat: \(lat), long: \(lon)")
-            
-            // show location icon here
-            self.locationImage.isHidden = false
-            
-            // API call using user current location ie. lat/long
-            self.showSpinner(onView: self.view)
-            weatherManager.fetchWeather(latitude: lat, longitude: lon)
-        }
-    }
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print(error)
-    }
-}
-
-//MARK: - Tableview Delegate
-extension MainViewController: UITableViewDelegate{
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 0
-    }
-}
-
-//MARK: - TableView Datasource
-extension MainViewController: UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allData.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MainViewTableCell", for: indexPath)
-        as! MainViewTableCell
-        
-        let data = allData[indexPath.row]
-        let weatherModel = WeatherModel(
-            conditionId: data.weather[0].id,
-            cityName: "",
-            temperature : 0,
-            latitude : 0,
-            longitude: 0)
-        cell.conditionImage.image = UIImage(systemName: weatherModel.conditionName)
-        
-        // convert miliseconds into date fromat
-        let date = Date(timeIntervalSince1970: (Double(allData[indexPath.row].dt)))
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "E, d MMM"
-        cell.dateLabel.text = String((dateFormatter.string(from: date)))
-        
-        cell.minTempLabel.text = String (format: "%.0f", allData[indexPath.row].temp.min)
-        cell.maxTempLabel.text = String (format: "%.0f", allData[indexPath.row].temp.max)
-        
-        cell.isUserInteractionEnabled = false
-        tableView.alwaysBounceVertical = false
-        return cell
-    }
-    
-}
-extension MainViewController: DailyWeatherManagerDelegate{
-    func didFailWithError(error: Error?, message: String?) {
-        print("Something is wrong: Error:\(error.debugDescription) ||| Message:\(message ?? "")")
-        self.removeSpinner()
-        
-        //TODO: show error dialog box with generic message
-    }
-    
-    func didGetAllResponse(dailyData: DailyWeatherData) {
-        allData = dailyData.daily
-        allData.removeFirst()
-        
-        DispatchQueue.main.async {
-            self.predictionTable.reloadData()
-            self.removeSpinner()
-        }
-    }
-}
-
